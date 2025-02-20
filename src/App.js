98d1516212ad61e01a6dc4fc52883823f9052f3a
Addhyan01 <addhyankumar@gmail.com>
2025-02-21 00:17:27 +0530

    create app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Courses from './pages/Courses';
+import Exams from './pages/Exams';
+import AttemptExam from './pages/AttemptExam';
+import Certificate from './pages/Certificate';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+function
